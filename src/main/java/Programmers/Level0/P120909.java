3d3fd7919eac54f26b8bc0772f1ec41e3b8c51d9
lllilyk <ym0402kkk@naver.com>
2023-11-08 23:47:50 +0900

    120909

--- src/main/java/Programmers/Level0/P120909.java
+++ src/main/java/Programmers/Level0/P120909.java
@@ -7,6 +7,7 @@ public class P120909 {
             // Math.sqrt() : 주어진 숫자의 제곱근을 계산하는 메소드
             double sqrtN = Math.sqrt(n);
 
+            /* Math.sqrt(2); // 1.414213562373095 가능 */
             if (sqrtN == (int)sqrtN) {
                 return 1;
             } else {
