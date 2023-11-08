345251a5da4fd8a006e8c96a7b464d9db3b24ff1
lllilyk <ym0402kkk@naver.com>
2023-11-08 22:31:14 +0900

    120851

--- src/main/java/Programmers/Level0/P120851.java
+++ src/main/java/Programmers/Level0/P120851.java
@@ -5,13 +5,10 @@ public class P120851 {
         public int solution(String my_string) {
             int answer = 0;
 
-            /* Character 클래스의 정적 메소드 - .isDigit(), .getNumericValue() */
             for (int i = 0; i < my_string.length(); i++) {
                 char c = my_string.charAt(i);
 
-                /* c가 문자이면 true 반환 */
                 if (Character.isDigit(c)) {
-                    /* 문자를 정수로 변환 */
                     int num = Character.getNumericValue(c);
                     answer += num;
                 }
