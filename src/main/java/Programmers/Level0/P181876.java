c0e3f8c028122c2d35389b6c636964af44ef146a
lllilyk <ym0402kkk@naver.com>
2023-11-09 22:22:45 +0900

    181876

---
+++ src/main/java/Programmers/Level0/P181876.java
@@ -0,0 +1,10 @@
+package Programmers.Level0;
+
+public class P181876 {
+    class Solution {
+        public String solution(String myString) {
+            String answer = myString.toLowerCase();
+            return answer;
+        }
+    }
+}
