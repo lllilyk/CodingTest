25949145f43d8b3ae1c41a4cf4f9dc30ac43574c
lllilyk <ym0402kkk@naver.com>
2023-11-11 20:23:45 +0900

    Math.sqrt()

--- src/main/java/Programmers/Level0/P120909.java
+++ src/main/java/Programmers/Level0/P120909.java
@@ -4,7 +4,8 @@ public class P120909 {
     class Solution {
         public int solution(int n) {
             // sqrtN : square root(제곱근)
-            // Math.sqrt() : 주어진 숫자의 제곱근을 계산하는 메소드
+            // Math 클래스
+            // .sqrt() : 주어진 숫자의 제곱근을 계산하는 메소드
             double sqrtN = Math.sqrt(n);
 
             /* Math.sqrt(2); // 1.414213562373095 가능 */
