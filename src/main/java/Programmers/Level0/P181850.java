a07594a08a16e8d153a7571441f826e9745120fa
lllilyk <ym0402kkk@naver.com>
2023-11-19 11:59:41 +0900

    Type casting

---
+++ src/main/java/Programmers/Level0/P181850.java
@@ -0,0 +1,10 @@
+package Programmers.Level0;
+
+public class P181850 {
+    class Solution {
+        public int solution(double flo){
+            int answer = (int) flo;
+            return answer;
+        }
+    }
+}
