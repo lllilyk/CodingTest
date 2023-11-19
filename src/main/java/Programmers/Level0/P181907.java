b9d6a5ac2065bbb7530481ffc16b94c65d8d02e8
lllilyk <ym0402kkk@naver.com>
2023-11-19 11:58:18 +0900

    substring(startIndex, endIndex)

--- src/main/java/Programmers/Level0/P181907.java
+++ src/main/java/Programmers/Level0/P181907.java
@@ -5,7 +5,13 @@ public class P181907 {
         public String solution(String my_string, int n) {
             String answer = "";
 
-            if (n >= 0 && n <= my_string.length()) {
+            if (n >= 1 && n <= my_string.length()) {
+
+                /* substring 메서드의 인덱스는 시작 인덱스(포함)부터 종료 인덱스(미포함)까지
+                 * substring(int beginIndex) / substring(int beginIndex, int endIndex)
+                 * ex) "Hello, World"
+                 * "Hello, World".substring(7) => "World"
+                 * "Hello, World".substring(0, 5) => "Hello" (0부터 4까지의 문자) */
                 answer = my_string.substring(0, n);
             }
 
